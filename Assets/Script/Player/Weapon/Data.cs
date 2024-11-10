1ffb615fdec07c13e3c41b8230a42dae43c20d88
Mohammad Darandeh <m.darande@gmail.com>
2024-11-09 21:26:18 -0500

    Update Data.cs

--- Assets/Script/Player/Weapon/Data.cs
+++ Assets/Script/Player/Weapon/Data.cs
@@ -1,10 +1,15 @@
 using System;
+using UnityEngine;
 
 namespace Script.Player.Weapon
 {
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Gun")]
     [Serializable]
-    public struct Data
+    public class Data: ScriptableObject
     {
-            
+        public GameObject projectilePrefab; 
+        public float fireRate = 1f; 
+        public float projectileSpeed = 20f; 
+        public int damage = 10; 
     }
 }
